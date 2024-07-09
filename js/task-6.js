d570d7f3f61bdcc8a81ac29e45c899b817c14c38
lehenkyi-yevhen-ar <meister.ramires@gmail.com>
2024-07-09 19:09:26 +0300

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -5,8 +5,7 @@ const boxContainer = document.querySelector('#boxes')
 
 create.addEventListener('click', createBoxes);
 destroy.addEventListener('click', () => {
-  boxContainer.innerHTML = ''})
-destroy.addEventListener('click', () => {
+  boxContainer.innerHTML = '';
   input.value = ''})
 
 function createBoxes() {
@@ -14,15 +13,19 @@ function createBoxes() {
 
   if (amount >=1 && amount <= 100) {
     boxContainer.innerHTML = '';
+    input.value = '';
     let boxSize = 30;
+
+    const documentFragment = document.createDocumentFragment('div');
     for(let i = 0; i < amount; i+=1) {
       const square = document.createElement('div');
       square.style.width = `${boxSize}px`;
       square.style.height = `${boxSize}px`;
       square.style.backgroundColor = getRandomHexColor();
-      boxContainer.appendChild(square);
+      documentFragment.appendChild(square);
       boxSize += 10;
     }
+    boxContainer.appendChild(documentFragment);
   }
 }
 
